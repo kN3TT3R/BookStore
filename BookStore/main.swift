81decc499dd2076725b743e179b39ec889920878
kN3TT3R <knetter@ptr-916yrbl8jp4st6yb1qx.18120a2.ip6.access.telenet.be>
2017-02-21 16:58:07 +0100

    Initial Commit

---
+++ BookStore/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BookStore
+//
+//  Created by Kenneth Debruyn on 21/02/17.
+//  Copyright © 2017 kN3TT3R. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
